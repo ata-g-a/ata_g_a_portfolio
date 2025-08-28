a4c3429e36a97d58a8aeb465463848c0e1abe253
ata ghaeem azar <58735880+ata-g-a@users.noreply.github.com>
2025-08-29 00:36:20 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-import preact from "@astrojs/preact";
-import sitemap from "@astrojs/sitemap"
-import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
-  integrations: [preact(), icon(), sitemap({
-    filter: (page) =>
-      !page.includes("/blog/tags") &&
-      !page.includes("/blog/techs"),
-  }),],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark'
-    },
-  },
+  site: 'https://YOUR_USERNAME.github.io/YOUR_REPO_NAME/',
+  base: '/YOUR_REPO_NAME/',
+  output: 'static',
 });
