f6fcb2e1a260e49035ac0554a7ccf1257600c104
ata-g-a <58735880+ata-g-a@users.noreply.github.com>
2025-08-29 21:27:38 +0330

    efit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import astroIcon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://ata-g-a.github.io/ata_g_a_portfolio/',
   base: '/ata_g_a_portfolio/',
   output: 'static',
-  integrations: [astroIcon()]
+  build: {
+    outDir: 'dist'
+  }
 });
